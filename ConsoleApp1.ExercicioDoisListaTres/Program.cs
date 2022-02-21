353de41e82e3e577f3a17a230beb20c87e18fb03
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-21 14:07:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioDoisListaTres/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioDoisListaTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 2 != 0)
+                    Console.WriteLine(i + " é impar");
+            }
+        }
+    }
+}
